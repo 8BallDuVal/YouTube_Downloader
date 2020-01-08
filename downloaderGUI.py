8b460809f0445e91a7d90243acd576d589cb51c7
Daniel <39527881+8BallDuVal@users.noreply.github.com>
2020-01-08 14:00:20 -0500

    Update downloaderGUI.py

--- downloaderGUI.py
+++ downloaderGUI.py
@@ -1,22 +1,30 @@
 import PySimpleGUI as sg
 import youtube_dl
 
-# Saving the YouTube video as an MP4
+# Method used to save the Youtube Video as an MP4
 def mp4(url, directory):
     ydl_opts = {
     'postprocessors': [{'key': 'FFmpegVideoConvertor',
                         'preferedformat': 'mp4',  # one of avi, flv, mkv, mp4, ogg, webm
                       }],
-    'outtmpl': directory+'\\%(title)s.%(ext)s',
-    }
+    'outtmpl': directory+'\\%(title)s.%(ext)s',                                             # Using given directory + name of youtube video as 
+    }                                                                                       # output destination
+    
+    # Obtaining YouTube Video's Information (mainly the title of the video for the popup after a successful download)
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        try:
+        info_dict = ydl.extract_info(url, download=False)
+        video_title = info_dict.get('title', None)
+    
+    # Downloading the Video as an MP4
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:                                                                                # Error handling 
             ydl.download([url])
-        except Exception as e:
-            msg = 'Unable to download video as an MP3 due to the error message above.'
+            sg.popup("The YouTube video you requested was downloaded successfully to the following location: ",directory+'/'+video_title+'.mp4')
+        except Exception as e:                                                              # popup displaying error message
+            msg = 'Unable to download video as an MP4 due to the error message above.'
             sg.popup(str(e),msg)
 
-# Saving the Youtube Video as an MP3
+# Method used to save the Youtube Video as an MP3
 def mp3(url, directory):
     ydl_opts = {
     'format': 'bestaudio/best',
@@ -27,36 +35,43 @@ def mp3(url, directory):
         'preferredquality': '192',
     }],
     }
+
+    # Obtaining YouTube Video's Information (mainly the title of the video for the popup after a successful download)
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        try:
+        info_dict = ydl.extract_info(url, download=False)
+        video_title = info_dict.get('title', None)
+
+    # Downloading the Video as an MP3
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:                                                                                # Error handling 
             ydl.download([url])
-        except Exception as e:
+            sg.popup("The YouTube video you requested was downloaded successfully to the following location: ",directory+'/'+video_title+'.mp3')
+        except Exception as e:                                                              # popup displaying error message
             msg = 'Unable to download video as an MP3 due to the error message above.'
             sg.popup(str(e),msg)
 
-sg.theme('DarkAmber')	# Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Enter a YouTube URL (https://www.youtube.com/...): '), sg.InputText()],
-            [sg.Text('Output Folder: ', size=(8, 1)), sg.Input(), sg.FolderBrowse()],
-            [sg.Button('Convert to MP3'),sg.Button('Convert to MP4')] ]
+sg.theme('DarkBrown4')	# Adding a touch of color using PySimpleGUI's built-in Themes
 
-# Create the Window
+# All the stuff inside the GUI window. 
+# Each list is a row, each entry in each list is a column. 
+layout = [  [sg.Text('Enter a YouTube URL (https://www.youtube.com/...):  '), sg.InputText()],      # First Row (Enter URL)
+            [sg.Text('Export to Output Folder: ', size=(39, 1)), sg.Input(), sg.FolderBrowse()],    # Second Row (Output Folder textbox + browse button)
+            [sg.Button('Convert to MP3'),sg.Button('Convert to MP4')] ]                             # Third Row (Convert to MP3/MP4 buttons)
+
+# Create the GUI Window
 window = sg.Window("Daniel's YouTube Downloader", layout)
 
-event, values = window.read()
-# Event Loop to process "events" and get the "values" of the inputs
-# event means the 'Convert to MP3' or 'Convert to MP4' button have been pressed
-# value[0] is URL and value[1] is directory
-# print('URL:' + url)
-# print('Directory:' + directory)
 while True:
+    # Obtaining the URL and directory information from URL in a loop
+    global url, directory
     event, values = window.read()
     url = values[0]
     directory = values[1]
+
     if event in (None, 'Exit'):	# if user closes window or presses Exit
         break
-    elif event in ('Convert to MP3'): #if the user clicks the "output folder" button
+    elif event in ('Convert to MP3'): # if the user clicks the "Convert to MP3" button
         mp3(url,directory)
-    elif event in ('Convert to MP4'):
+    elif event in ('Convert to MP4'): # if the user clicks the "Convert to MP4" button
         mp4(url,directory)
 window.close()
